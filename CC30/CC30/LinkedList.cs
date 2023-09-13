3308e8e4fd55c47eea7ea5324b3c1baf71eb3b12
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-09-13 18:28:37 +0300

    finidhing

---
+++ CC30/CC30/LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC30
+{
+	public class LinkedList<T>
+	{
+		public Node<T> Head { get; set; }
+
+
+		public LinkedList() { }
+
+		public LinkedList(T value)
+		{
+			Node<T> node = new Node<T>(value);
+
+			Head = node;
+		}
+
+		public void Insert(T value)
+		{
+			Node<T> node = new Node<T>(value);
+			if (Head == null)
+			{
+				Head = node;
+			}
+			else
+			{
+				node.Next = Head;
+				Head = node;
+			}
+		}
+
+	}
+}
---
+++ CC30/CC30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using CC30;
+
+HashMap hashMap = new HashMap(10);
+
+hashMap.Set("Ahmad", "Civil Engineer");
+hashMap.Set("Osama", "Mechanical Engineer");
+hashMap.Set("Zaid", "Doctor");
+hashMap.Set("Mohammad", "CS");
+hashMap.Set("Bashar", "CIS");
+hashMap.Set("Anas", "SE");
+hashMap.Set("Ala'a", "Math");
+hashMap.Set("Abdullah", "Chemistry");
+hashMap.Set("Yaman", "Physics");
+hashMap.Set("Said", "CS");
+
+hashMap.Print();
+Console.WriteLine($"Said:{hashMap.Get("Said")}");
+Console.WriteLine($"Ala'a:{hashMap.Get("Ala'a")}");
+Console.WriteLine($"Fares:{hashMap.Get("Fares")}");
+
+Console.WriteLine($"Said:{hashMap.Has("Said")}");
+Console.WriteLine($"Ala'a:{hashMap.Has("Ala'a")}");
+Console.WriteLine($"Fares:{hashMap.Has("Fares")}");
+Collection<string> collection = new Collection<string>();
+collection = hashMap.Keys();
+Console.WriteLine(collection[0]);
+
